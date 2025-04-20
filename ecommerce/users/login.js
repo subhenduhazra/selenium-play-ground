48a871a060ebf5aaeb32f0352b9a46372b30ccae
Subhendu Hazra <subhendu965.hazra@gmail.com>
2025-04-20 23:27:54 +0530

    third party login

--- ecommerce/users/login.js
+++ ecommerce/users/login.js
@@ -23,19 +23,23 @@ function login(){
         const currentUser = getUser(email);
         if(currentUser){
             if(currentUser.email == email && currentUser.password == password){
-                loginUser(currentUser);
-                //redirect or move to dash
-                if(redirectUrl){
-                    window.location = redirectUrl;
-                }else{
-                    locateToDashboard();
-                }
+                loginAndRedirect(currentUser, redirectUrl); //call
             } else{
                 logError("Incorrect email or password.");
             }
         }else{
             logError("Incorrect email or password.");
         }
+    }
+}
+
+function loginAndRedirect(currentUser, redirectUrl){
+    loginUser(currentUser);
+    //redirect or move to dash
+    if(redirectUrl){
+        window.location = redirectUrl;
+    }else{
+        locateToDashboard();
     }
 }
 
@@ -47,5 +51,27 @@ function validatePasswordEmpty(){
 }
 
 function thirdPartyLogin(){
-    window.open("thirdPartyLogin.html","_blank","height=550,width=750,top=50,left=50");
+    window.open("thirdPartyLogin.html?client-id=eCommerce","_blank","height=550,width=750,top=50,left=50");
+}
+
+///event from thirparty popup
+window.addEventListener("message",(e)=>{
+    console.log(e);
+    const data = e?.data;
+    handleThirdPartylogin(data);
+})
+
+function handleThirdPartylogin(data){
+    if(!data || data.loginStatus == "fail" || !data.email || data.email.trim().length == 0 ){
+        logError("Third party login failed")
+        return;
+    }
+    const thpEmail = data.email;
+    const userWithThp = getUser(thpEmail);
+    if(userWithThp && userWithThp.email == thpEmail){
+        loginAndRedirect(userWithThp, redirectUrl);
+    }else{
+        users.push({email:thpEmail, name:data.name});
+        saveAllUsers(users);
+    }
 }
---
+++ ecommerce/users/thirdPartyLogin.js
@@ -0,0 +1,29 @@
+//redirect if thirdPartyLoginClient is not set 
+const urlParams = new URLSearchParams(window.location.search);
+const cliendId = urlParams.get('client-id');
+if(!cliendId){
+    document.getElementsByTagName("body")[0].innerHTML="<h1>Client Id not set or invalid, contact to admin/support</h1>"
+}
+
+function thirdParty_popup_Login(){
+    const name = document.getElementById("thirdPartyLogin_name").value;
+    if(!name){
+        alert("Name can't be empty!");
+        return;
+    }
+
+    const email = document.getElementById("thirdPartyLogin_email").value;
+    if(!email){
+        alert("Email can't be empty!");
+        return;
+    }
+
+    let msg = {};
+    if(email == "parna@tcs.com" || email == "pragati@tcs.com"){
+        msg = {loginStatus:'fail'};
+    }else{
+        msg = {loginStatus:'success',name, email};
+    }
+    window.opener.postMessage(msg,'*');
+    window.close('','_parent','');
+}
